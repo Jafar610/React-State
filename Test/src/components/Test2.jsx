59379da5db51883b11efbf8a7440d0cad498fb84
Jafar610 <jafarmohamed955@gmail.com>
2026-03-15 17:20:28 +0300

    file updated

---
+++ Test/src/components/Test2.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class Test2 extends Component {
+    constructor (){
+        super();
+        this.state = {
+            count : 0,
+        }
+    }
+
+    Add = ()=>{
+
+    }
+
+
+  render() {
+    return (
+      <>
+      <div>
+        <h1>The Value of count is {this.state.count}</h1>
+        <button onClick={this.Add}>Click</button>
+      </div>
+      </>
+    )
+  }
+}
+export default Test2
